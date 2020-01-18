a222bdc408db236ec012c21ccaf1d93c2f085617
Gahyki <jason.k99@hotmail.com>
2020-01-18 18:06:15 -0500

    restructured menu bar

--- qrapp/src/App.js
+++ qrapp/src/App.js
@@ -5,24 +5,52 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Drawer variant="permanent" width="240">
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <header className="App-header">
-        <label>I like it</label>
-        <input type="checkbox" />
-      </header>
-    </div>
-  );
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
 }
 
-export default App;
+function TempDrawer(){
+  const [state, setState] = React.useState({
+    left: false
+  });
+  const toggleDrawer = (open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, left: open });
+  };
+  return(
+    <Drawer open={state.left} onClose={toggleDrawer(false)}>
+      <List component="nav">
+        <ListItem button>
+          <ListItemText primary="Home"/>
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Scan"/>
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Profile"/>
+        </ListItem>
+        <ListItemLink href="https://my.concordia.ca/psp/upprpr9/?cmd=login&languageCd=ENG&">
+          <ListItemText primary="MyConcordia"/>
+        </ListItemLink>
+      </List>
+    </Drawer>
+  );
+}
+export default class App extends React.Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <label>I like it</label>
+          <input type="checkbox" />
+        </header>
+      </div>
+    );
+  }
+}
