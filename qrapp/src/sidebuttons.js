0181ea21d16189de2381c412b3877834eb50587f
welleh <kevinjiang64@hotmail.com>
2020-01-19 07:16:05 -0500

    commit suicide

---
+++ qrapp/src/sidebuttons.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function homeButton() {
+	document.getElementById("l1").innerHTML = "HOME";
+	document.getElementById("l2").innerHTML = "HOME";
+	document.getElementById("l3").innerHTML = "HOME";
+	document.getElementById("l4").innerHTML = "HOME";
+	document.getElementById("l5").innerHTML = "HOME";
+}
