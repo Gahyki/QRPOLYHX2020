922d847867f7ebe0ca2fb1de6828d86e7f27b8ba
Gahyki <jason.k99@hotmail.com>
2020-01-18 21:43:26 -0500

    I failed at life

--- qrapp/src/App.js
+++ qrapp/src/App.js
@@ -3,53 +3,61 @@ import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import "./App.css";
 
+
+//Menu Item
 function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
 
-function TempDrawer(){
-  const [state, setState] = React.useState({
-    left: false
-  });
-  const toggleDrawer = (open) => event => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, left: open });
-  };
-  return(
-    <Drawer open={state.left} onClose={toggleDrawer(false)}>
-      <List component="nav">
-        <ListItem button>
-          <ListItemText primary="Home"/>
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Scan"/>
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Profile"/>
-        </ListItem>
-        <ListItemLink href="https://my.concordia.ca/psp/upprpr9/?cmd=login&languageCd=ENG&">
-          <ListItemText primary="MyConcordia"/>
-        </ListItemLink>
-      </List>
-    </Drawer>
-  );
-}
-export default class App extends React.Component{
+class MenuItem extends React.Component{
   constructor(){
     super();
   }
   render(){
     return(
+      <List component="nav">
+        <ListItem button id="c1">
+          <ListItemText primary="Home"/>
+        </ListItem>
+        <ListItem button id="c2">
+          <ListItemText primary="Scan"/>
+        </ListItem>
+        <ListItem button id="c3">
+          <ListItemText primary="Profile"/>
+        </ListItem>
+        <ListItemLink id="c4" href="https://my.concordia.ca/psp/upprpr9/?cmd=login&languageCd=ENG&">
+          <ListItemText primary="MyConcordia"/>
+        </ListItemLink>
+      </List>
+    )
+  }
+}
+
+export default class App extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
       <div className="App">
-        <header className="App-header">
-          <label>I like it</label>
-          <input type="checkbox" />
-        </header>
+          <div id="menu">
+            <Drawer variant="permanent" width="240">
+             <MenuItem/>
+            </Drawer>
+          </div>
+        <div id="content">
+          <header className="App-header">
+            <label>I like it</label>
+            <input type="checkbox" />
+          </header>
+        </div>
       </div>
     );
   }
